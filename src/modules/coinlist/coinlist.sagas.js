804355543cf631c43fdb3081067684b7da7eff73
Shaun Church <shaun@shaunchurch.com>
2017-08-27 17:12:02 +0100

    Remove incorrect saga typing

--- src/modules/coinlist/coinlist.sagas.js
+++ src/modules/coinlist/coinlist.sagas.js
@@ -1,14 +1,15 @@
+// @flow
 import { all, call, put, takeLatest } from 'redux-saga/effects';
 import CoinMarketCap from '../../services/CoinMarketCap';
-import type { Action, Coin } from './coinlist.types';
+import type { Action, ApiCoin } from './coinlist.types';
 
 function* watchFetchCoins() {
   yield takeLatest('coinlist/REQ_COINS', fetchCoins);
 }
 
-export const fetchCoins: * = function*(action: Action) {
+export const fetchCoins = function*(action: Action) {
   try {
-    const apiCoins: Array<Coin> = yield call(CoinMarketCap.ticker);
+    const apiCoins: Array<ApiCoin> = yield call(CoinMarketCap.ticker);
     yield put({ type: 'coinlist/RES_COINS_SUCCESS', payload: apiCoins });
   } catch (e) {
     yield put({ type: 'coinlist/RES_COINS_FAILED', message: e.message });
