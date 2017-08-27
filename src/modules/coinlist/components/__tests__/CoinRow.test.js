fcaba644091c17b9dbbc0f6137e0321ceacb1bd9
Shaun Church <shaun@shaunchurch.com>
2017-08-27 19:43:45 +0100

    Add <Price /> test

--- src/modules/coinlist/components/__tests__/CoinRow.test.js
+++ src/modules/coinlist/components/__tests__/CoinRow.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 import toJson from 'enzyme-to-json';
 import CoinRow from '../CoinRow';
 import CoinJSON from '../../../../testutils/data/Coin.json';
 import { Price } from '../styled';
+import theme from '../../../../theme';
+import 'jest-styled-components';
 
 describe('<CoinRow />', () => {
   it('should render a coin row positive', () => {
@@ -16,13 +19,15 @@ describe('<CoinRow />', () => {
     expect(toJson(wrapper)).toMatchSnapshot();
   });
 
-  it('should render in the correct style when moon', () => {
-    const wrapper = shallow(<Price moon={true} />);
-    expect(toJson(wrapper)).toMatchSnapshot();
+  it('should render <Price /> in the correct style when moon', () => {
+    const tree = toJson(shallow(<Price moon={true} theme={theme} />));
+    expect(tree).toMatchSnapshot();
+    expect(tree).toHaveStyleRule('color', theme.green);
   });
 
   it('should render <Price /> in the correct style when no moon', () => {
-    const wrapper = shallow(<Price moon={false} />);
-    expect(toJson(wrapper)).toMatchSnapshot();
+    const tree = toJson(shallow(<Price moon={false} theme={theme} />));
+    expect(tree).toMatchSnapshot();
+    expect(tree).toHaveStyleRule('color', theme.red);
   });
 });
